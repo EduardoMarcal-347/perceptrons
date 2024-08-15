d088597632a49941d6159969f18d9c40709bdf5e
Eduardo Marçal <eduardo.filho@wisebyte.com.br>
2024-08-15 00:41:35 -0300

    improving perceptrons learning

--- src/perceptron_digitos/Aprendizagem.java
+++ src/perceptron_digitos/Aprendizagem.java
@@ -1,77 +1,101 @@
 package perceptron_digitos;
-/**
- *
- * @author Clarimundo
- */
+
 public class Aprendizagem {
-    
-    private double x[][]={{1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,1},
-                          {1,0,1,0,1,1,0,0,1,0,0,1,0,1,1,1}};
-   
-    private double w[]= new double [16];
-    private double t[]={1,-1};       
+
+    private final double[][] numbersImg ={
+            {1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,1},
+            {1,0,1,0,1,1,0,0,1,0,0,1,0,1,1,1},
+            {1,1,1,1,0,0,1,1,1,1,1,0,0,1,1,1},
+            {1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1},
+            {1,1,0,1,1,0,1,1,1,1,0,0,1,0,0,1},
+            {1,1,1,1,1,0,0,1,1,1,0,0,1,1,1,1},
+            {1,1,1,1,1,0,0,1,1,1,1,0,1,1,1,1},
+            {1,1,1,1,0,0,1,0,0,1,0,0,1,0,0,1},
+            {1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,1},
+            {1,1,1,1,1,0,1,1,1,1,0,0,1,1,1,1}
+    };
+
+    private final double[][] weight = new double [16][4];
+    private final double[][] target = {
+            {1, 1, 1, 1},
+            {1, 1, 1, -1},
+            {1, 1, -1, 1},
+            {1, 1, -1, -1},
+            {1, -1, 1, 1},
+            {1, -1, 1, -1},
+            {1, -1, -1, 1},
+            {1, -1, -1, -1},
+            {-1, 1, 1, 1},
+            {-1, 1, 1, -1}
+    };
     private int epocas;
-        
-    public Aprendizagem(){  
+
+    public Aprendizagem(){
         epocas=0;
     }
-    
-    public double [] getw(){
-        return w;
+
+    public double [][] getWeight(){
+        return weight;
     }
-    
-    public double [] gett(){
-        return t;
-    }    
-    
-    public int getepocas(){
+
+    public double [][] getTarget(){
+        return target;
+    }
+
+    public int getEpocas(){
         return epocas;
     }
-    
-    public double somatorio(int i){
-        double yent=0;       
+
+    public double somatorio(int i, int k){
+        double yent=0;
         for(int j=0;j<16;j++)
-            yent = yent + x[i][j] * w[j];
+            yent = yent + numbersImg[i][j] * weight[j][k];
         return yent;
     }
 
     public double saida(double yent, double limiar){
-       double f;
-       
-        if(yent > limiar)
-            f = 1;
-        else 
-            if(yent < -limiar)
-                f = -1;          
-            else
-                f = 0;
-       return f;
+        return (yent > limiar) ? 1 : (yent < -limiar) ? -1 : 0;
     }
-    
-    public void atualiza(double alfa, double f[]){
-            for(int i=0;i<2;i++)
-                for(int j=0; j<16; j++)
-                     w[j] = w[j] + alfa * (t[i]-f[i]) * x[i][j];
+
+    public void atualiza( double alfa, double[] f, int ePos){
+        for(int i=0;i<4;i++)
+            for(int j=0; j<16; j++)
+                weight[j][i] = weight[j][i] + alfa * (target[ePos][i]-f[i]) * numbersImg[ePos][j];
     }
-    
+
     public void algoritmo(double alfa, double limiar){
-       double yent;
-       double f[] = {0,0};
-       boolean mudou;       
-       
-       for(int j=0;j<16;j++)  // zerar os pesos
-               w[j]=0;       
-       do{
-           mudou=false;
-           for(int i=0;i<2;i++){   // dois padrões de entrada (digito 0) e (digito 1)
-              yent = somatorio(i);
-              f[i] = saida(yent,limiar);
-              if(f[i] != t[i])                  
-                  mudou=true;
-           }
-           if(mudou==true)
-              atualiza(alfa,f); 
-           epocas++;
-       }while(mudou==true);      
-    }      
+        double yent;
+        double[] result = {0,0,0,0};
+        boolean mudou;
+
+        for (int i=0;i<16;i++)
+            for (int j=0; j<4; j++)
+                weight[i][j]=0; // zerar os pesos
+
+        do{
+            mudou = false;
+            for (int i = 0; i < 10; i++) {   // 10 padrões de entrada (digito 0) ~ (digito 9)
+                for (int j = 0; j < 4; j++) {
+                    yent = somatorio(i,j);
+                    result[j] = saida(yent, limiar);
+                    if (result[j] != target[i][j]) {
+                        mudou = true;
+                    }
+                }
+                if (mudou) {
+                    atualiza(alfa, result, i);
+                }
+            }
+            epocas++;
+        }while(mudou);
+
+        for(double[] arr : weight) {
+            System.out.print("[ ");
+            for(double element: arr) {
+                System.out.print(element +", ");
+            }
+            System.out.println("]");
+        }
+
+    }
 }
