f4e2bf76c5a4a7e34d602b2ab271375b74e0c9e2
Eduardo Marçal <eduardo.filho@wisebyte.com.br>
2024-08-15 00:41:51 -0300

    refactoring validation

--- src/perceptron_digitos/Validacao.java
+++ src/perceptron_digitos/Validacao.java
@@ -1,54 +1,48 @@
 package perceptron_digitos;
-/**
- *
- * @author Clarimundo
- */
+
 public class Validacao {
-    
-    public Validacao(){
-        
+
+    public Validacao() {
     }
-    
-    public double somatorio(int mat[][], double w[]){
-        
-        double yent=0;  
-        double entrada[] = new double[16];
-        int l=1;
-        entrada[0]=1;
-        for(int i=0; i<5; i++)
-            for(int j=0; j<3; j++){
+
+    public double somatorio(int[][] mat, double[][] weight, int k) {
+        double yent = 0;
+        double[] entrada = new double[16];
+        int l = 1;
+        entrada[0] = 1;
+        for (int i = 0; i < 5; i++)
+            for (int j = 0; j < 3; j++) {
                 entrada[l] = mat[i][j];
                 l++;
-            }        
-        for(int j=0;j<16;j++)
-            yent = yent + entrada[j]*w[j];
+            }
+        for (int j = 0; j < 16; j++)
+            yent = yent + entrada[j] * weight[j][k];
         return yent;
     }
 
-    public double saida(double yent, double limiar){
-        double f;
-        
-        if(yent > limiar)
-            f = 1;
-        else 
-            if(yent < -limiar)
-                f = -1;          
-            else
-                f = 0;
-        return f;
+    public double saida(double yent, double limiar) {
+        return (yent > limiar) ? 1 : (yent < -limiar) ? -1 : 0;
     }
-    
-    public String teste(int mat[][], double w[], double t[], double limiar){
-              
-       double yent = somatorio(mat,w);
-       double f = saida(yent,limiar);
-       if(f==t[0])
-           return "0";
-       else
-           if(f==t[1])
-               return "1";
-           else
-               return "?";             
-    }             
-    
+
+    public String teste(int[][] mat, double[][] weight, double[][] target, double limiar) {
+        double[] results = new double[4];
+        for (int j = 0; j < 4; j++) {
+            double yent = somatorio(mat, weight, j);
+            results[j] = saida(yent, limiar);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            boolean match = true;
+            for (int j = 0; j < 4; j++) {
+                if (results[j] != target[i][j]) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) {
+                return String.valueOf(i);
+            }
+        }
+        return "?";
+    }
 }
